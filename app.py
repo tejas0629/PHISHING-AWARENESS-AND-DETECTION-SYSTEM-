5008554b63780fb078ece5083955fd28fd7cd2e8
TEJAS UPRETI <tejasupreti2964@gmail.com>
2026-05-12 19:04:04 +0530

    change smtp port to 2525

--- app.py
+++ app.py
@@ -17,20 +17,15 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY", "secret123")  # fallback if not set
-
 app.config.update(
-    MAIL_SERVER=os.getenv("MAIL_SERVER"),
-    MAIL_PORT=int(os.getenv("MAIL_PORT", 587)),
+    MAIL_SERVER="smtp-relay.brevo.com",
+    MAIL_PORT=2525,
     MAIL_USE_TLS=True,
     MAIL_USE_SSL=False,
-    MAIL_SUPPRESS_SEND=False,
-    MAIL_DEBUG=True,
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
     MAIL_DEFAULT_SENDER=os.getenv("MAIL_USERNAME"),
-    MAIL_MAX_EMAILS=None,
-    MAIL_ASCII_ATTACHMENTS=False,
-    MAIL_TIMEOUT=int(os.getenv("MAIL_TIMEOUT", 120))
+    MAIL_TIMEOUT=120
 )
 mail = Mail(app)
 # -------------------- Utilities --------------------
